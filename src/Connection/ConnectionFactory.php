ab4d83dbcedf06fbe22114265f2990af2a157159
Lynh Tran <jenky.w0w@gmail.com>
2018-12-28 00:54:29 +0700

    wip

---
+++ src/Connection/ConnectionFactory.php
@@ -0,0 +1,169 @@
+<?php
+
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\Ring\Future\CompletedFutureArray;
+use Illuminate\Contracts\Container\Container;
+use Psr\Http\Message\ResponseInterface;
+
+class ConnectionFactory
+{
+    /**
+     * Map configuration array keys with ES ClientBuilder setters.
+     *
+     * @var array
+     */
+    protected $configMappings = [
+        'ssl_verification' => 'setSSLVerification',
+        'sniff_on_start' => 'setSniffOnStart',
+        'retries' => 'setRetries',
+        'http_handler' => 'setHandler',
+        'connection_pool' => 'setConnectionPool',
+        'connection_selector' => 'setSelector',
+        'serializer' => 'setSerializer',
+        'connection_factory' => 'setConnectionFactory',
+        'endpoint' => 'setEndpoint',
+        'namespaces' => 'registerNamespace',
+    ];
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * Create a new connection factory instance.
+     *
+     * @param  \Illuminate\Contracts\Container\Container  $container
+     * @return void
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Make the Elasticsearch client for the given named configuration, or
+     * the default client.
+     *
+     * @param  array $config
+     * @return \Elasticsearch\Client|mixed
+     */
+    public function make(array $config)
+    {
+        return $this->createClient($config);
+    }
+
+    /**
+     * Build and configure an Elasticsearch client.
+     *
+     * @param  array $config
+     * @return \Elasticsearch\Client
+     */
+    protected function buildClient(array $config): Client
+    {
+        $clientBuilder = ClientBuilder::create();
+
+        // Configure hosts
+        $clientBuilder->setHosts($config['hosts'] ?? []);
+
+        foreach ($this->configMappings as $key => $method) {
+            $value = Arr::get($config, $key);
+
+            if (is_array($value)) {
+                foreach ($value as $val) {
+                    $clientBuilder->{$method}($val);
+                }
+            } elseif ($value !== null) {
+                $clientBuilder->{$method}($value);
+            }
+        }
+
+        $this->configureLogging($clientBuilder, $config);
+        $this->configureAwsHandlers($clientBuilder, $config);
+
+        return $clientBuilder->build();
+    }
+
+    /**
+     * Configure Elasticsearch client logging.
+     *
+     * @param  \Elasticsearch\ClientBuilder $client
+     * @param  array $config
+     * @return void
+     */
+    protected function configureLogging(ClientBuilder $client, array $config)
+    {
+        // Configure logging
+        if (Arr::get($config, 'logging.enable')) {
+            $logChannel = Arr::get($config, 'logging.channel');
+            $logPath = Arr::get($config, 'logging.path');
+            $logLevel = Arr::get($config, 'logging.level');
+
+            if ($logChannel) {
+                $clientBuilder->setLogger($this->container['log']->channel($logChannel));
+            } elseif ($logPath && $logLevel) {
+                $logObject = ClientBuilder::defaultLogger($logPath, $logLevel);
+                $clientBuilder->setLogger($logObject);
+            }
+        }
+    }
+
+    /**
+     * Configure handlers for any AWS hosts.
+     *
+     * @param  \Elasticsearch\ClientBuilder $client
+     * @param  array $config
+     * @return void
+     */
+    protected function configureAwsHandlers(ClientBuilder $client, array $config)
+    {
+        foreach ($config['hosts'] ?? [] as $host) {
+            if (isset($host['aws']) && $host['aws']) {
+                $clientBuilder->setHandler(function (array $request) use ($host) {
+                    $psr7Handler = \Aws\default_http_handler();
+                    $signer = new \Aws\Signature\SignatureV4('es', $host['aws_region']);
+                    $request['headers']['Host'][0] = parse_url($request['headers']['Host'][0])['host'];
+
+                    // Create a PSR-7 request from the array passed to the handler
+                    $psr7Request = new Request(
+                        $request['http_method'],
+                        (new Uri($request['uri']))
+                            ->withScheme($request['scheme'])
+                            ->withHost($request['headers']['Host'][0]),
+                        $request['headers'],
+                        $request['body']
+                    );
+
+                    // Sign the PSR-7 request with credentials from the environment
+                    $signedRequest = $signer->signRequest(
+                        $psr7Request,
+                        new \Aws\Credentials\Credentials($host['aws_key'], $host['aws_secret'])
+                    );
+
+                    // Send the signed request to Amazon ES
+                    $response = $psr7Handler($signedRequest)
+                        ->then(function (ResponseInterface $response) {
+                            return $response;
+                        }, function ($error) {
+                            return $error['response'];
+                        })
+                        ->wait();
+
+                    // Convert the PSR-7 response to a RingPHP response
+                    return new CompletedFutureArray([
+                        'status' => $response->getStatusCode(),
+                        'headers' => $response->getHeaders(),
+                        'body' => $response->getBody()->detach(),
+                        'transfer_stats' => ['total_time' => 0],
+                        'effective_url' => (string) $psr7Request->getUri(),
+                    ]);
+                });
+            }
+        }
+    }
+}
--- src/ElasticsearchServiceProvider.php
+++ src/ElasticsearchServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace Jenky\LaravelElasticsearch;
 
-use Cviebrock\LaravelElasticsearch\ServiceProvider as ElasticsearchProvider;
+use Elasticsearch\Connections\ConnectionFactory;
+use Illuminate\Support\ServiceProvider;
 
-class ElasticsearchServiceProvider extends ElasticsearchProvider
+class ElasticsearchServiceProvider extends ServiceProvider
 {
     /**
      * Register any package services.
@@ -13,8 +14,32 @@ class ElasticsearchServiceProvider extends ElasticsearchProvider
      */
     public function register()
     {
-        if (! $this->app->bound(ElasticsearchProvider::class)) {
-            $this->app->register(ElasticsearchProvider::class);
-        }
+        $this->registerConnectionServices();
+    }
+
+    /**
+     * Register the primary elasticsearch bindings.
+     *
+     * @return void
+     */
+    protected function registerConnectionServices()
+    {
+        // The connection factory is used to create the actual connection instances on
+        // the elasticsearch. We will inject the factory into the manager so that it may
+        // make the connections while they are actually needed and not of before.
+        $this->app->singleton('elasticsearch.factory', function ($app) {
+            return new ConnectionFactory($app);
+        });
+
+        // The database manager is used to resolve various connections, since multiple
+        // connections might be managed. It also implements the connection resolver
+        // interface which may be used by other components requiring connections.
+        // $this->app->singleton('db', function ($app) {
+        //     return new DatabaseManager($app, $app['db.factory']);
+        // });
+
+        // $this->app->bind('db.connection', function ($app) {
+        //     return $app['db']->connection();
+        // });
     }
 }
